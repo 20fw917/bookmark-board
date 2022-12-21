6fd510af1169a29e185cdf005677667531b8b9d4
20fw917 <20fw917@gmail.com>
2022-12-21 20:29:15 +0900

    Initial Commit

---
+++ src/main/java/com/project/bookmarkboard/BookmarkBoardApplication.java
@@ -0,0 +1,13 @@
+package com.project.bookmarkboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bookmarkboard/BookmarkBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bookmarkboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
